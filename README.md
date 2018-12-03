1d06e851bae645c65aa5c35a5821dee00044ea07
Bipin Paul <iambipinpaul@outlook.com>
2018-12-03 15:26:43 +0545

    Add update Startup

---
+++ README.md
@@ -0,0 +1,6 @@
+# BlazorServerSide
+
+1. **src/BlazorServerSide.Api**: Sample Api project from ASP.NET CORE default template  
+2. **src/BlazorServerSide.App**: Blazor Applicaion 
+3. **src/BlazorServerSide.Server**: ASPNET CORE that hosts Blazor App
+4. **src/BlazorServerSide.ServiceProxy**: project contaning csharp Client generated using nswag (`ServiceProxyClient.cs`)
--- src/BlazorServerSide.App/Startup.cs
+++ src/BlazorServerSide.App/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using BlazorServerSide.App.Services;
 using BlazorServerSide.ServiceProxy;
+using System.Net.Http;
 
 namespace BlazorServerSide.App
 {
@@ -11,7 +12,9 @@ namespace BlazorServerSide.App
         {
             // Since Blazor is running on the server, we can use an application service
             // to read the forecast data.
+            services.AddSingleton<System.String>();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<HttpClient>();
             services.AddScoped<Client, Client>();
         }
 
